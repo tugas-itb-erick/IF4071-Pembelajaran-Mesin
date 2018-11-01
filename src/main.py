3a5564540e7723807da58a7e32b33753b2c3aaa3
wijayaerick <wijaya.erick52@gmail.com>
2018-11-01 22:16:27 +0700

    keras

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+from fnn.keras import FnnKeras
+import pandas as pd
 
+data_pd = pd.read_csv("iris.data.txt", header=None)
+labels = data_pd[4].values
+del data_pd[4]
+data = data_pd.values
+
+model = FnnKeras([10, 20]).fit(data, labels, epochs=25)
+print(model.evaluate(data, labels))
+print(model.predict(data))
